7ff864bc0c1992a148b5eae9978cb3c1db060f29
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-14 17:20:45 +0200

    Ändrat URL för databasen, ändrat koden så den matchar mongoose-schemat med rätt versaler/gemener på namn.

--- src/js/experiences.js
+++ src/js/experiences.js
@@ -4,7 +4,7 @@
 import { deleteExperience } from './delete.js';
 import { fillEditForm } from './edit.js';
 
-export const url = "https://backend-moment2-1.onrender.com/api/workexperience"; // Lagrar url för API, lägger till export
+export const url = "https://backend-moment3.onrender.com/api/workexperience"; // Lagrar url för API, lägger till export
 
 // Asynkron funktion för att hämta data (exporteras som modul)
 export async function fetchExperiences() {
@@ -29,9 +29,9 @@ function displayExperiences(experiences) {
 
     // Sorterar erfarenheterna baserat på startdatum med den senast påbörjade erfarenheten först
     experiences.sort((a, b) => {
-        if (a.startdate < b.startdate) {
+        if (a.startDate < b.startDate) {
             return 1; // Om a är mindre (tidigare datum) än b, sortera a efter b
-        } else if (a.startdate > b.startdate) {
+        } else if (a.startDate > b.startDate) {
             return -1; // Om a är större (senare datum) än b, sortera a före b
         } else {
             return 0; // Om a och b är samma, behåll den nuvarande ordningen
@@ -45,27 +45,27 @@ function displayExperiences(experiences) {
         experiences.forEach(experience => {
 
             // Använder substring för att endast inkludera de 10 första tecknen i datumet (börjar på index 0)
-            const startDate = experience.startdate.substring(0, 10);
+            const startDate = experience.startDate.substring(0, 10);
 
             let endDate = "Pågående"; // Sätter slutdatum till "Pågående" initalt (om den är null)
 
             // Kontrollerar om enddate finns och inte är null
-            if (experience.enddate) {
-                endDate = experience.enddate.substring(0, 10); // Inkluderar datumets första 10 tecken
+            if (experience.endDate) {
+                endDate = experience.endDate.substring(0, 10); // Inkluderar datumets första 10 tecken
             }
 
             // Skapar en article för varje jobberfarenhet
             const articleEl = document.createElement("article");
 
             // Skapar ett unikt ID för varje article baserat på erfarenhetens ID
-            let articleID = `${experience.id}`;
+            let articleID = `${experience._id}`;
             articleEl.id = articleID;
 
             // Sätter artikelns innehåll till erfarenhetens data (företagsnamn, titel, plats, datum och beskrivning)
             articleEl.innerHTML = `
             <div>
-            <h3 class="company-name">${experience.companyname}</h3>
-            <p><strong>Roll:</strong> ${experience.jobtitle}</p>
+            <h3 class="company-name">${experience.companyName}</h3>
+            <p><strong>Roll:</strong> ${experience.jobTitle}</p>
             <p><strong>Plats:</strong> ${experience.location}</p>
             <p><strong>Tidsperiod:</strong> ${startDate} - ${endDate}</p>
             <h4>Beskrivning</h4>
